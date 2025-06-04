923c26be5ed5c44adff1d34b28654d224517b552
IMTIAZ <126008482+IMTIAZ-X@users.noreply.github.com>
2025-06-04 23:09:55 +0600

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+/* main.c (Shared core logic for desktop) */
 #include <SDL.h>
 #include <stdio.h>
 
@@ -7,9 +8,9 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    SDL_Window* window = SDL_CreateWindow("Cross-Platform SDL2 App",
-        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-        640, 480, SDL_WINDOW_SHOWN);
+    SDL_Window* window = SDL_CreateWindow("Cross Platform App",
+                                          SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                                          480, 320, SDL_WINDOW_SHOWN);
 
     if (!window) {
         printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
@@ -17,23 +18,18 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-
-    SDL_SetRenderDrawColor(renderer, 0, 128, 255, 255); // Light blue
-    SDL_RenderClear(renderer);
-    SDL_RenderPresent(renderer);
-
     SDL_Event e;
     int quit = 0;
+
     while (!quit) {
         while (SDL_PollEvent(&e)) {
             if (e.type == SDL_QUIT) {
                 quit = 1;
             }
         }
+        SDL_Delay(10);
     }
 
-    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
